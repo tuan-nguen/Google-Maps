03efbd99daa9f56b71bbe10c83cce2c6922798f4
tuan-nguen <50369971+tuan-nguen@users.noreply.github.com>
2019-07-23 17:39:07 +0300

    Add files via upload

--- hello.php
+++ hello.php
@@ -23,8 +23,38 @@ if (!$db_selected) {
   die ('Can\'t use db : ' . mysqli_error());
 }
 
+$stringCoordinates = file_get_contents('/var/www/html/coordinates.txt');
+/** For testing purposes
+if($stringCoordinates){
+  echo $stringCoordinates;
+}
+*/
+
+/** The .php file reads from a .txt file
+  * and pulls the coordinates from it */
+
+$array = explode(",", $stringCoordinates);
+$lat1 = $array[0];
+$lat2 = $array[2];
+$lng1 = $array[1];
+$lng2 = $array[3];
+
+/** For testing purposes
+echo "\n";
+echo $lat1;
+echo "\n";
+echo $lat2;
+echo "\n";
+echo $lng1;
+echo "\n";
+echo $lng2;
+echo "\n";
+*/
+
+
 // Select all the rows in the markers table
-$query = "SELECT * FROM markers WHERE 1";
+$query = "SELECT city, country, lat, lng FROM newTable WHERE lat >= ' " . $lat1 . " ' AND lng >= ' " . $lng1 . " '
+                                                       AND lat <= ' " . $lat2 . " ' AND lng <= ' " . $lng2 . " ' ";
 $result = mysqli_query($connection, $query);
 if (!$result) {
   die('Invalid query: ' . mysqli_error());
@@ -40,8 +70,8 @@ $ind=0;
 while ($row = @mysqli_fetch_assoc($result)){
   // Add to XML document node
   echo '<marker ';
-  echo 'id="' . $row['id'] . '" ';
-  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'city="' . $row['city'] . '" ';
+  echo 'country="' . parseToXML($row['country']) . '" ';
   echo 'lat="' . $row['lat'] . '" ';
   echo 'lng="' . $row['lng'] . '" ';
   echo '/>';
@@ -50,5 +80,4 @@ while ($row = @mysqli_fetch_assoc($result)){
 
 // End XML file
 echo '</markers>';
-
 ?>
